ca87778a71a46da66e756ced535f388d21b12020
3120004920陈峥 <2364074108@qq.com>
2022-09-29 16:03:13 +0800

    loginOK

--- src/main/java/com/chenzheng/reggie/controller/EmployeeController.java
+++ src/main/java/com/chenzheng/reggie/controller/EmployeeController.java
@@ -1,10 +1,21 @@
 package com.chenzheng.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.chenzheng.reggie.common.R;
+import com.chenzheng.reggie.entity.Employee;
 import com.chenzheng.reggie.service.EmployeeService;
+import com.chenzheng.reggie.service.impl.EmployeeServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @RestController
@@ -12,4 +23,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+        if(null==emp){
+            return R.error("找不到该用户");
+        }
+        if(!emp.getPassword().equals(password)){
+            {
+                return R.error("密码错误");
+            }
+        }
+        if(emp.getStatus()==0){
+            return R.error("该用户已被禁用");
+        }
+        request.getSession().setAttribute("employee",emp.getId());
+        return R.success(emp);
+    }
 }
